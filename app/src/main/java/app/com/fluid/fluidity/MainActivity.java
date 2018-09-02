d8ff15a524a9994fce52a553c2ff1120aa2ca0dd
appljeet <abhijitsingh.student@gmail.com>
2018-09-02 14:41:17 -0700

    Updated screen layout and got rid of button

--- app/src/main/java/app/com/fluid/fluidity/MainActivity.java
+++ app/src/main/java/app/com/fluid/fluidity/MainActivity.java
@@ -1,10 +1,7 @@
 package app.com.fluid.fluidity;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,9 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.button);
-        tv.setText(stringFromJNI());
+
     }
 
     /**
